bb694ead2d915c0257687693a0c3db87377c418d
Sudhanshu Mishra <mailsudhanshumishra@gmail.com>
2024-10-13 22:03:12 +1100

    Updates to use netlify cli

--- upload-netlify.sh
+++ upload-netlify.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-curl --header "Content-Type: application/zip" --header "Authorization: Bearer ${NETLIFY_TOKEN}" --data-binary output.zip --url https://api.netlify.com/api/v1/sites/sudhanshutheone.netlify.com/deploys
+#curl --header "Content-Type: application/zip" --header "Authorization: Bearer ${NETLIFY_TOKEN}" --data-binary output.zip --url https://api.netlify.com/api/v1/sites/sudhanshutheone.netlify.com/deploys
+# Install Netlify CLI
+npm install -g netlify-cli
+
+# Authenticate Netlify CLI using the token
+export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
+
+# Deploy the site
+netlify deploy --site 05f90622-e089-411a-a051-723a8cae32db --prod --dir=output
